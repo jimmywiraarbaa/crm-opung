946b8a22fb53f6be5b457f871647313f1cd0920b
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-29 13:22:19 +0700

    Membuat Fitur Tambah ke keranjang

--- resources/views/show_product.blade.php
+++ resources/views/show_product.blade.php
@@ -18,6 +18,11 @@
         <form action="{{ route('edit_product', $product) }}" method="get">
             <button type="submit">Ubah Produk</button>
         </form>
+        <form action="{{ route('add_to_cart', $product) }}" method="post">
+            @csrf
+            <input type="number" name="amount" value="1">
+            <button type="submit">Tambah ke keranjang</button>
+        </form>
     </body>
 
 </html>

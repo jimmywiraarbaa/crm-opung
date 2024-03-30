5496d70cdcd9a87fe54fb597a638bb742c387f3c
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-30 22:46:46 +0700

    Membuat Tempat untuk mengirimkan bukti pembayaran pesanan

--- resources/views/show_order.blade.php
+++ resources/views/show_order.blade.php
@@ -15,6 +15,16 @@
             <p>Produk: {{ $transaction->product->name }}</p>
             <p>Banyak: {{ $transaction->amount }}</p>
         @endforeach
+        @if ($order->is_paid == false && $order->payment_receipt == null)
+            <form action="{{ route('submit_payment_receipt', $order) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <label for="payment_receipt">Upload Bukti Bayar</label>
+                <br>
+                <input type="file" name="payment_receipt" id="payment_receipt">
+                <br>
+                <button type="submit">Konfirmasi Pembayaran</button>
+            </form>
+        @endif
     </body>
 
 </html>

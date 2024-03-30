b114c45796102e27b1af6b3d9ee1e91985507886
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-03-30 21:39:08 +0700

    Membuat Halaman untuk menampilkan semua orderan

---
+++ resources/views/index_order.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Order</title>
+    </head>
+
+    <body>
+        @foreach ($orders as $order)
+            <p>ID : {{ $order->id }}</p>
+            <p>by : {{ $order->user->name }}</p>
+            <p>{{ $order->created_at }}</p>
+        @endforeach
+    </body>
+
+</html>

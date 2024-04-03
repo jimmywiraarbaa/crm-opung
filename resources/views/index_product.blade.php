12b3d792d97759d376f367e05300da8689827714
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-04-04 00:42:21 +0700

    Mengatur tampilan pada mobile, tablet dan desktop

--- resources/views/index_product.blade.php
+++ resources/views/index_product.blade.php
@@ -28,37 +28,59 @@
                             <div class="d-flex flex-row justify-content-around my-3" role="group"
                                 aria-label="Basic example">
                                 <a href="{{ route('index_product') }}"
-                                    class="border rounded px-5 py-2 nav-link{{ !request()->has('category') ? ' text-white bg-primary' : '' }}">Semua</a>
+                                    class="border rounded px-lg-5 py-lg-2 px-md-4 py-md-2 px-3 py-1 nav-link{{ !request()->has('category') ? ' text-white bg-primary' : '' }}">Semua</a>
                                 <a href="{{ route('index_product', ['category' => 'Makanan']) }}"
-                                    class="border rounded px-5 py-2 nav-link{{ request()->category === 'Makanan' ? ' text-white bg-primary' : '' }}">Makanan</a>
+                                    class="border rounded px-lg-5 py-lg-2 px-md-4 py-md-2 px-3 py-1 nav-link{{ request()->category === 'Makanan' ? ' text-white bg-primary' : '' }}">Makanan</a>
                                 <a href="{{ route('index_product', ['category' => 'Minuman']) }}"
-                                    class="border rounded px-5 py-2 nav-link{{ request()->category === 'Minuman' ? ' text-white bg-primary' : '' }}">Minuman</a>
+                                    class="border rounded px-lg-5 py-lg-2 px-md-4 py-md-2 px-3 py-1 nav-link{{ request()->category === 'Minuman' ? ' text-white bg-primary' : '' }}">Minuman</a>
                             </div>
 
-                            <div class="card-group m-auto">
-                                @foreach ($products as $product)
-                                    <div class="card m-3" style="width: 18rem">
-                                        <img class="card-img-top" src="{{ url('storage/' . $product->image) }}"
-                                            alt="">
-                                        <div class="card-body">
-                                            <p class="card-text fs-5  fw-semibold ">{{ $product->name }}</p>
-                                            <p class="card-text"><sup>Rp</sup>{{ $product->price }}</p>
-                                            <div class="d-flex justify-content-between">
-                                                <form action="{{ route('show_product', $product) }}" method="get">
-                                                    <button class="btn btn-primary" type="submit">Detail</button>
-                                                </form>
-                                                @if (Auth::check() && Auth::user()->is_admin)
-                                                    <form action="{{ route('delete_product', $product) }}" method="post">
-                                                        @method('delete')
-                                                        @csrf
-                                                        <button class="btn btn-danger" type="submit">Hapus</button>
-                                                    </form>
-                                                @endif
+                            <div class="card-group m-auto px-2 px-md-4 ">
+                                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                                    @foreach ($products as $product)
+                                        <div class="col mb-md-4 mb-2">
+                                            <div class="card d-flex flex-md-column flex-row">
+                                                <div class="row">
+                                                    <div
+                                                        class="col-md-12 col-4 d-flex justify-content-center align-items-center text-center">
+                                                        <img class="card-img-top rounded-0 ms-md-0 ms-3"
+                                                            src="{{ url('storage/' . $product->image) }}" alt="">
+                                                    </div>
+                                                    <div class="col-md-12 col-8">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title ">{{ $product->name }}</h5>
+                                                            <p class="card-text"><sup>Rp</sup>{{ $product->price }}</p>
+                                                            <div class="d-flex justify-content-between">
+                                                                <div class="">
+                                                                    @if (Auth::check() && Auth::user()->is_admin)
+                                                                        <form
+                                                                            action="{{ route('delete_product', $product) }}"
+                                                                            method="post">
+                                                                            @method('delete')
+                                                                            @csrf
+                                                                            <button class="btn btn-danger"
+                                                                                type="submit">Hapus</button>
+                                                                        </form>
+                                                                    @endif
+                                                                </div>
+                                                                <div class="d-flex justify-content-end align-items-end">
+                                                                    <form action="{{ route('show_product', $product) }}"
+                                                                        method="get">
+                                                                        <button class="btn btn-primary fw-bold"
+                                                                            type="submit">+</button>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                @endforeach
+                                    @endforeach
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>

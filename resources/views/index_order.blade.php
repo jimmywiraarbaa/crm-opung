a9e93c82b39d10ea3d54878234e0f6dca27cac0f
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-06-01 02:12:10 +0700

    Mengkonfigurasi timezone waktu menjadi WIB dan memperbaiki view pada index order

--- resources/views/index_order.blade.php
+++ resources/views/index_order.blade.php
@@ -18,25 +18,52 @@
                         <div class="card">
                             <div class="card-header">{{ __('Orders') }}</div>
 
-                            <div class="card-body m-auto">
-                                @foreach ($orders as $order)
-                                    <div class="card mb-2" style="width: 30rem">
+                            <div class="card-body">
+                                @foreach ($orders->reverse() as $order)
+                                    <div class="card mb-2">
                                         <div class="card-body">
                                             <a href="{{ route('show_order', $order) }}">
                                                 <h5 class="card-title">Order ID {{ $order->id }}</h5>
                                             </a>
 
                                             <h6 class="card-subtitle mb-2 text-muted">by {{ $order->user->name }}</h6>
-                                            <p>{{ $order->created_at }}</p>
+                                            <p>{{ $order->created_at->isoFormat('D MMMM YYYY') }}</p>
+                                            <p>{{ $order->created_at->isoFormat('HH:mm') }} WIB</p>
                                             @if ($order->is_paid == true)
                                                 <p class="card-text text-success">Terbayar</p>
                                             @else
                                                 <p class="card-text text-danger">Belum Bayar</p>
                                                 @if ($order->payment_receipt)
                                                     <div class="d-flex flex-row justify-content-around">
-                                                        <a class="btn btn-primary"
-                                                            href="{{ url('storage/' . $order->payment_receipt) }}">Tampilkan
-                                                            Kwintansi</a>
+                                                        <button type="button" class="btn btn-primary"
+                                                            data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                                            Tampilkan Kwitansi
+                                                        </button>
+                                                        {{-- Modal --}}
+                                                        <div class="modal fade" id="exampleModal" tabindex="-1"
+                                                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                            <div class="modal-dialog">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                                                            Bukti Pembayaran</h1>
+                                                                        <button type="button" class="btn-close"
+                                                                            data-bs-dismiss="modal"
+                                                                            aria-label="Close"></button>
+                                                                    </div>
+                                                                    <div class="modal-body">
+                                                                        {{-- Tampilkan Gambar payment receipt disini --}}
+                                                                        <img src="{{ url('storage/' . $order->payment_receipt) }}"
+                                                                            class="img-fluid" alt="Bukti Pembayaran">
+                                                                    </div>
+                                                                    <div class="modal-footer">
+                                                                        <button type="button" class="btn btn-danger"
+                                                                            data-bs-dismiss="modal">Close</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        {{-- Modal --}}
                                                         @if (Auth::user()->is_admin)
                                                             <form action="{{ route('confirm_payment', $order) }}"
                                                                 method="post">
@@ -60,6 +87,7 @@
                     </div>
                 </div>
             </div>
+
         @endsection
     </body>
 
